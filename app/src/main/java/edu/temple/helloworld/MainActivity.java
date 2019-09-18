4e6205854a655f63826d90dc341e5e028b1cd57b
callmemars <marcelrmillan@gmail.com>
2019-09-18 12:13:53 -0400

    second

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package edu.temple.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button disButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        disButton = findViewById(R.id.clickButton);
+        disButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                //Toast toast = Toast.makeText(getApplicationContext() , "You have clicked the button", Toast.LENGTH_LONG);
+                //toast.show();
+                TextView greetingText = (TextView)findViewById(R.id.greetingText);
+                greetingText.setText("You have clicked the button");
+            }
+        });
+    }
+
+
+}
